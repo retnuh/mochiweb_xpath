548db65a2d8ede0ea265759d1febfe681ecf5b23
David JULIEN <david@semiocast.com>
2011-07-29 10:55:21 +0200

    Remove an invalid guard when result is directly a boolean or a number

--- src/mochiweb_xpath.erl
+++ src/mochiweb_xpath.erl
@@ -324,7 +324,7 @@ remove_positions(Nodes) when is_list(Nodes) ->
     [ remove_positions(SubNode) || SubNode <- Nodes ];
 remove_positions({Tag, Attrs, Children, _}) ->
     {Tag, Attrs, remove_positions(Children)};
-remove_positions(Data) when is_binary(Data) ->
+remove_positions(Data) ->
     Data.
 
 %% @doc Get node according to a position relative to root node
